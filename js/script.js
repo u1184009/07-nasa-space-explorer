8ef38c4b9621f339f0bb09a24dc59f371864addd
Mike Minh Pham <u1184009@umail.utah.edu>
2026-07-10 02:56:18 +0000

    API KEY ADDED

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const startInput = document.getElementById('startDate');
 const endInput = document.getElementById('endDate');
 
+const API_KEY = 'TCAMnBXTO6phdlrzbCGZ4tBEWsFUKp006ufQizL8';
+
 // Call the setupDateInputs function from dateRange.js
 // This sets up the date pickers to:
 // - Default to a range of 9 days (from 9 days ago to today)
